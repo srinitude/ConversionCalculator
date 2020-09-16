1c0038a0d4eea45e4096bdd0167a47314155fd88
Kiren Srinivasan <srinitude@gmail.com>
2020-09-15 20:06:48 -0400

    Adds simple logic to convert from unit to unit

--- ConversionCalculator/ContentView.swift
+++ ConversionCalculator/ContentView.swift
@@ -42,9 +42,31 @@ struct ContentView: View {
     private var currentDestinationUnit: String {
         possibleDestinationUnits[currentDestinationUnitIndex]
     }
+    private var intermediaryUnit: String {
+        "meters"
+    }
     
     private var convertedSourceToDestinationNumber: Double {
-        return 0
+       getDestinationNumber(fromSourceUnit: currentSourceUnit, fromSourceNumber: numberOfSourceUnits, withDestinationUnit: currentDestinationUnit)
+    }
+    
+    private func getDestinationNumber(fromSourceUnit sourceUnit: String, fromSourceNumber sourceNumber: Double, withDestinationUnit destinationUnit: String) -> Double {
+        let conversions: [String: Double] = [
+            "meters_to_\(intermediaryUnit)": 1,
+            "kilos_to_\(intermediaryUnit)": 1000,
+            "feet_to_\(intermediaryUnit)": 0.3048,
+            "yards_to_\(intermediaryUnit)": 0.9144,
+            "miles_to_\(intermediaryUnit)": 1609.34,
+            "\(intermediaryUnit)_to_kilos": 0.001,
+            "\(intermediaryUnit)_to_feet": 3.28084,
+            "\(intermediaryUnit)_to_yards": 1.09361,
+            "\(intermediaryUnit)_to_miles": 0.000621371
+        ]
+        
+        guard let conversionFromSourceToIntermediary = conversions["\(sourceUnit)_to_\(intermediaryUnit)"] else { return 0 }
+        guard let conversionFromIntermediaryToDestination = conversions["\(intermediaryUnit)_to_\(destinationUnit)"]
+            else { return 0 }
+        return conversionFromSourceToIntermediary * conversionFromIntermediaryToDestination
     }
     
     // MARK: - UI
@@ -71,7 +93,7 @@ struct ContentView: View {
             }
             
             Section(header: Text("Answer")) {
-                Text("\(convertedSourceToDestinationNumber, specifier: "%.2f") \(currentDestinationUnit)")
+                Text("\(convertedSourceToDestinationNumber, specifier: "%.6f") \(currentDestinationUnit)")
             }
         }
     }
